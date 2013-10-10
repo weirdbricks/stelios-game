0ab693b73b8f3b594be769187d203926cca8603c
Charlie Root <weirdbricks@opera.com>
2013-10-10 02:02:21 -0700

    table drawing done

--- start1.rb
+++ start1.rb
@@ -4,13 +4,31 @@ require 'sinatra'
 set :bind, '0.0.0.0'
 set :port, 80
 
-height=10
-width=10
-
 def draw_table(height,width)
-
+  table = ""
+  table = table + "<style>
+  tr
+  {
+     height:75px;
+  }
+  td
+  {
+     width:75px;
+  }
+  </style>"
+  table = table + "<center><table>\n\r"
+  for y in 1..height
+	  table = table + "<tr>"
+		for x in 1..width
+			table = table + "<td>" + "#{y},#{x}" + "</td>"
+		end
+	  table = table + "</tr>\n\r"
+  end
+  table = table + "</table>\n\r"
+  return table
 end
 
 
 get '/' do
+	draw_table(9,9)	
 end
